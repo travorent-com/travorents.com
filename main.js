cbe713f69793da8b36bc4556666f1429d96dce37
Travorent <travorent.com@gmail.com>
2026-08-03 16:50:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://127.0.0.1:3000'
-  : 'https://75wl7kbyfr.c38.paasapps.net';
+  : 'https://api.travorents.com';
 
 
 const booking =
